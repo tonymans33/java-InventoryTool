14d19e4b413d6935aacbf32ae938169a5eae74cb
Tonymans33 <tonymansour66@yahoo.com>
2021-01-23 01:12:58 +0200

    Making the search method general : searching by instrument spec & changing the add method

--- src/Inventory.java
+++ src/Inventory.java
@@ -13,13 +13,7 @@ public class Inventory {
     // Function to add instrument
     public void addInstrument(String serialNumber, double price, InstrumentSpec spec){
 
-        Instrument instrument = null;
-
-        if(spec instanceof GuitarSpec){
-            instrument = new Guitar(serialNumber, price, (GuitarSpec)spec);
-        }else if (spec instanceof MandolinSpec){
-            instrument = new Mandolin(serialNumber, price, (MandolinSpec)spec);
-        }
+        Instrument instrument = new Instrument(serialNumber, price, spec);
 
         inventory.add(instrument);
     }
@@ -37,37 +31,24 @@ public class Inventory {
         return null;
     }
 
-    // Function to search for a spec guitar and return list of the matched guitars
-    public List search(GuitarSpec searchSpec){
+    // Function to search for a spec instrument and return list of the matched instruments
+    public List search(InstrumentSpec searchSpec){
 
-        List matchingGuitars  = new LinkedList(); // Making a linked list of the guitars that match with the given spec
+        List matchingInstruments  = new LinkedList(); // Making a linked list of the guitars that match with the given spec
         for (Iterator i = inventory.iterator(); i.hasNext() ;){
-            if (i instanceof Guitar){
-                Guitar guitar = (Guitar)i.next();
-                if(guitar.getSpec().matches(searchSpec)){ // Calling the matches function to check if match
-                    matchingGuitars.add(guitar);
-                }
+
+            Instrument instrument = (Instrument) i.next();
+            if(instrument.getSpec().matches(searchSpec)){ // Calling the matches function to check if match
+                matchingInstruments.add(instrument);
             }
 
         }
 
-        return matchingGuitars; // return a list of all matching guitars
+        return matchingInstruments; // return a list of all matching guitars
     }
 
-    // Function to search for a spec mandolin and return list of the matched mandolins
-    public List search(MandolinSpec searchSpec){
 
-        List matchingMandolins  = new LinkedList(); // Making a linked list of the mandolins that match with the given spec
-        for (Iterator i = inventory.iterator(); i.hasNext() ;){
 
-            Mandolin mandolin = (Mandolin) i.next();
-            if(mandolin.getSpec().matches(searchSpec)){ // Calling the matches function to check if match
-                matchingMandolins.add(mandolin);
-            }
 
-        }
-
-        return matchingMandolins; // return a list of all matching guitars
-    }
 
 }
